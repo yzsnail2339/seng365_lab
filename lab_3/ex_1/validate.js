4e1baa252116b8372692acc7d5ccb7aedbca1ea9
snail <zya49@uclive.ac.nz>
2023-07-06 05:01:54 +1200

    add more exercise

---
+++ lab_3/ex_1/validate.js
@@ -0,0 +1,10 @@
+function validateForm(){
+    var search_string =
+        document.getElementById("search_string").value;
+    if (search_string == "") {
+        alert("Search string is empty!");
+        return false;
+    } else {
+        return true;
+    }
+}
